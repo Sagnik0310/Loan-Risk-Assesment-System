c3cb14b49420867808e7cd07a374f6d3afddf881
anantika2004 <anantika2004@gmail.com>
2026-07-23 17:54:38 +0530

    fetch data

---
+++ database/fetch_data.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+from database.mongodb import MongoDBConnection
+from database.collections import Collections
+
+
+class FetchData:
+
+    def init(self):
+        self.mongo = MongoDBConnection()
+
+    def get_dataframe(self):
+        
+
+        try:
+            # Connect to MongoDB
+            db = self.mongo.connect()
+
+            # Select collection
+            collection = db[Collections.LOAN_DATA]
+
+            # Fetch all documents
+            data = list(collection.find())
+
+            # Convert to DataFrame
+            df = pd.DataFrame(data)
+
+            # Remove MongoDB ObjectId column
+            if "_id" in df.columns:
+                df.drop(columns=["_id"], inplace=True)
+
+            return df
+
+        except Exception as e:
+            print(f"Error fetching data: {e}")
+            return pd.DataFrame()
+
+        finally:
+            self.mongo.close()
+
+
+if name == "main":
+
+    fetcher = FetchData()
+
+    df = fetcher.get_dataframe()
+
+    print(df.head())
+
+    print("\nShape :", df.shape)
+
+    print("\nColumns :")
+
+    print(df.columns.tolist())
